27ab256495944bc1afc6cfe447e77560164f53c6
gunwooko <kkw722@gmail.com>
2020-10-22 16:20:20 +0900

    make useInput

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
 
 // Use react-hooks => useState
+
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setValue(value);
+    console.log(event.target);
+  };
+  return { value, onChange };
+};
+
 const App = () => {
   const [item, setItem] = useState(1);
+  const name = useInput("Mr.");
 
   const add = () => {
     setItem(item + 1);
@@ -18,6 +32,9 @@ const App = () => {
       <h2>Start editing to see some magic happen!</h2>
       <button onClick={add}>BUTTON</button>
       <button onClick={minus}>BUTTON</button>
+      <br />
+      <input placeholder="Name" value={name.value} onChange={name.onChange} />
+      {/* you can use {...name} instead of value={name.value} ... */}
     </div>
   );
 };
