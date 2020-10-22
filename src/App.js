fe27462928b6504119a43d9d39162df8246fdc9b
gunwooko <kkw722@gmail.com>
2020-10-22 15:25:59 +0900

    practice useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+// Use react-hooks => useState
+const App = () => {
+  const [item, setItem] = useState(1);
+
+  const add = () => {
+    setItem(item + 1);
+  };
+
+  const minus = () => {
+    setItem(item - 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {item} </h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={add}>BUTTON</button>
+      <button onClick={minus}>BUTTON</button>
     </div>
   );
+};
+
+// Class component version
+class ClassVersion extends React.Component {
+  state = {
+    item: 1,
+  };
+
+  add = () => {
+    this.setState((state) => {
+      return { item: state.item + 1 };
+    });
+  };
+
+  minus = () => {
+    this.setState((state) => {
+      return { item: state.item - 1 };
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello {this.state.item} </h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <button onClick={this.add}>BUTTON</button>
+        <button onClick={this.minus}>BUTTON</button>
+      </div>
+    );
+  }
 }
 
 export default App;
