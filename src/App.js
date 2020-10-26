0847012ab2c86d3f587434fe90368a7541324444
gunwooko <kkw722@gmail.com>
2020-10-26 13:52:12 +0900

    make useTabs with useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 // Use react-hooks => useState
-
 const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
@@ -14,16 +13,30 @@ const useInput = (initialValue, validator) => {
     }
     if (willUpdate) {
       setValue(value);
+      console.log(value);
     }
     console.log(event.target);
   };
   return { value, onChange };
 };
 
+// Use react-hooks => useState
+const content = [
+  { tab: "Section 1", content: "I'm the content of the section 1" },
+  { tab: "Section 2", content: "I'm the content of the section 2" },
+  { tab: "Section 3", content: "I'm the content of the section 3" },
+];
+
+const useTabs = (initialValue, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialValue);
+  return { currentItem: allTabs[currentIndex], changeItem: setCurrentIndex };
+};
+
 const App = () => {
-  const [item, setItem] = useState(1);
+  const [item, setItem] = useState(0);
   const maxLen = (value) => value.length <= 10;
   const name = useInput("Mr.", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
 
   const add = () => {
     setItem(item + 1);
@@ -40,40 +53,47 @@ const App = () => {
       <button onClick={add}>BUTTON</button>
       <button onClick={minus}>BUTTON</button>
       <br />
+
       <input placeholder="Name" value={name.value} onChange={name.onChange} />
       {/* you can use {...name} instead of value={name.value} ... */}
+      <br />
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
+      <br />
     </div>
   );
 };
 
 // Class component version
-class ClassVersion extends React.Component {
-  state = {
-    item: 1,
-  };
+// class ClassVersion extends React.Component {
+//   state = {
+//     item: 1,
+//   };
 
-  add = () => {
-    this.setState((state) => {
-      return { item: state.item + 1 };
-    });
-  };
+//   add = () => {
+//     this.setState((state) => {
+//       return { item: state.item + 1 };
+//     });
+//   };
 
-  minus = () => {
-    this.setState((state) => {
-      return { item: state.item - 1 };
-    });
-  };
+//   minus = () => {
+//     this.setState((state) => {
+//       return { item: state.item - 1 };
+//     });
+//   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello {this.state.item} </h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <button onClick={this.add}>BUTTON</button>
-        <button onClick={this.minus}>BUTTON</button>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello {this.state.item} </h1>
+//         <h2>Start editing to see some magic happen!</h2>
+//         <button onClick={this.add}>BUTTON</button>
+//         <button onClick={this.minus}>BUTTON</button>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
